974d0744f44340c2e421998d0893c87152c6d6c3
Morytala <theBest@Morytalas-Mac.local>
2014-12-14 23:50:31 -0800

    Initial Commit

---
+++ Git Test/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  Git Test
+//
+//  Created by Morytala on 12/14/14.
+//  Copyright (c) 2014 Morytala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
